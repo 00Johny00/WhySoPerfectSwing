dbf2674ed02cdb7ae06a58f3b852eee6095ae4fc
00Johny00 <jan.jagiello00@gmail.com>
2022-01-21 22:39:54 +0100

    2nd commit

--- target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.doskonala"},{"l":"com.mycompany.doskonala.Controller"},{"l":"com.mycompany.doskonala.Model"},{"l":"com.mycompany.doskonala.View"}];updateSearchResults();
